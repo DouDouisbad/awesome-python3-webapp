73641a49ca96e0d97b715a341df4ae28ff7c686a
DouDouisbad <jinlei950324@163.com>
2019-02-10 08:08:35 +0800

    day11
    学习廖同学文章
    
    Co-Authored-By: doudouisbad <doudouisbad@users.noreply.github.com>

--- www/config.py
+++ www/config.py
@@ -1,32 +1,37 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Nov  9 11:22:06 2018
 
-@author: SAB
-"""
+'''
+Configuration
+'''
+
+__author__ = 'Michael Liao'
+
+import config_default
 
 class Dict(dict):
     '''
-    simple dict but support access as x,y style.
+    Simple dict but support access as x.y style.
     '''
-    def __init__(self, names= (), values =(), **kw):
+    def __init__(self, names=(), values=(), **kw):
         super(Dict, self).__init__(**kw)
         for k, v in zip(names, values):
-            self[k]= v
-    
+            self[k] = v
+
     def __getattr__(self, key):
         try:
             return self[key]
         except KeyError:
             raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
     def __setattr__(self, key, value):
         self[key] = value
-    
+
 def merge(defaults, override):
     r = {}
     for k, v in defaults.items():
         if k in override:
-            if isinstance(v, dict):  
+            if isinstance(v, dict):
                 r[k] = merge(v, override[k])
             else:
                 r[k] = override[k]
@@ -49,6 +54,3 @@ except ImportError:
     pass
 
 configs = toDict(configs)
-
-        
-    
--- www/config_default.py
+++ www/config_default.py
@@ -4,17 +4,18 @@ Created on Fri Nov  9 11:10:23 2018
 
 @author: SAB
 """
-config = {
+configs = {
         'debug': True,
         'db':{
                 'host':'127.0.0.1',
-                'port':'3306',
-                'user':'www',
-                'password': 'www',
+                'port':3306,
+                'user':'www-data',
+                'password': 'www-data',
                 'db':'awesome'
                 },
         'session':{
-                'sercet': 'Awesome'
+        'secret': 'Awesome'
                 }
         }
+        
         
